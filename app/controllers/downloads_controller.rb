ae495d148ebbf65a694c934c0e4e09e75c3d3d17
Howard Ding <hding2@illinois.edu>
2017-08-28 14:59:41 -0500

    Do all stream stuff possible purely on the Java side.

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -4,6 +4,7 @@ class DownloadsController < ApplicationController
   #include ActionController::Streaming
 
   java_import java.io.BufferedOutputStream
+  java_import java.io.BufferedInputStream
   java_import java.util.zip.ZipOutputStream
   java_import java.util.zip.ZipEntry
   java_import java.io.PipedOutputStream
@@ -33,8 +34,10 @@ class DownloadsController < ApplicationController
         pipe = PipedOutputStream.new
         result = PipedInputStream.new
         pipe.connect(result)
-        buffered_stream = BufferedOutputStream.new(pipe)
+        buffered_result = BufferedInputStream.new(result, 8192)
+        buffered_stream = BufferedOutputStream.new(pipe, 8192)
         zip_stream = ZipOutputStream.new(buffered_stream)
+        zip_stream.set_level(0)
         #zip_stream.set_method(ZipOutputStream::STORED)
         manifest = File.open(@request.manifest_path)
         copy_thread = Thread.new do
@@ -53,13 +56,18 @@ class DownloadsController < ApplicationController
         response.headers['Content-Type'] = 'application/zip'
         #TODO: for request 9, with 1M files, this keeps ending early - can we make it not?
         #Maybe try reading the java stream directly - need to findout how to deal with byte arrays in jruby to do this
+        buffer = Java::byte[8192].new
+        while (bytes_read = buffered_result.read(buffer)) != -1
+          response.stream.write buffer.to_a.first(bytes_read).pack('C*')
+        end
+        
+        # # This wasn't working properly
+        # ruby_result = result.to_io
         # while bytes = ruby_result.read(8192)
         #   response.stream.write bytes
         # end
-        buffer = Java::byte[8192].new
-        while (bytes_read = result.read(buffer)) != -1
-          response.stream.write buffer.to_a.first(bytes_read).pack('C*')
-        end
+        
+        # #This was stopping before we finished; I don't know why
         #ruby_result = result.to_io
         # begin
         #   while bytes = ruby_result.readpartial(8192)
@@ -68,8 +76,6 @@ class DownloadsController < ApplicationController
         # rescue EOFError
         #   #do nothing - we're done
         # end
-        # #send_data result.read, filename: "#{@request.zip_name}.zip", disposition: 'attachment',
-        #          type: 'application/zip', stream: true
       ensure
         response.stream.close
       end
