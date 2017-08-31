b7ed85b0ad774e9af34bea525a193657522bd24b
Howard Ding <hding2@illinois.edu>
2017-08-31 12:29:03 -0500

    Needs some cleanup, but removing some of the buffering and finding some jruby specific ways to handle data type conversions really helps.

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -28,15 +28,17 @@ class DownloadsController < ApplicationController
     end
   end
 
+
+  BUFFER_SIZE=1024
   def download
     if @request.ready?
       begin
         pipe = PipedOutputStream.new
         result = PipedInputStream.new
         pipe.connect(result)
-        buffered_result = BufferedInputStream.new(result, 8192)
-        buffered_stream = BufferedOutputStream.new(pipe, 8192)
-        zip_stream = ZipOutputStream.new(buffered_stream)
+#        buffered_result = BufferedInputStream.new(result, BUFFER_SIZE)
+#        buffered_stream = BufferedOutputStream.new(pipe, BUFFER_SIZE)
+        zip_stream = ZipOutputStream.new(pipe)
         zip_stream.set_level(0)
         #zip_stream.set_method(ZipOutputStream::STORED)
         manifest = File.open(@request.manifest_path)
@@ -56,9 +58,10 @@ class DownloadsController < ApplicationController
         response.headers['Content-Type'] = 'application/zip'
         #TODO: for request 9, with 1M files, this keeps ending early - can we make it not?
         #Maybe try reading the java stream directly - need to findout how to deal with byte arrays in jruby to do this
-        buffer = Java::byte[8192].new
-        while (bytes_read = buffered_result.read(buffer)) != -1
-          response.stream.write buffer.to_a.first(bytes_read).pack('C*')
+        buffer = Java::byte[BUFFER_SIZE].new
+        while (bytes_read = result.read(buffer)) != -1
+#          response.stream.write buffer.to_a.first(bytes_read).pack('C*')
+          response.stream.write String.from_java_bytes(buffer).first(bytes_read)
         end
         
         # # This wasn't working properly
